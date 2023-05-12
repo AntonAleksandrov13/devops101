ba841312df64525247c0fa77a7b3179cda4115cb
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2023-05-12 21:34:39 +0200

    Working version of Flask application with tests and requirements

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello_world():
+        return "Hello, World!"
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
---
+++ app/tests/test.py
@@ -0,0 +1,23 @@
+import pytest as pytest
+from app import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def test_root(client):
+    response = client.get("/")
+    assert b"Hello, World!" in response.data
+
+
+def test_failing_root(client):
+    response = client.get("/")
+    assert b"Hello World!" not in response.data
