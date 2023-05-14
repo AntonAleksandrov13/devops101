a4b0cdf273686c15397bde33c1885c29d78bf75c
AntonAleksandrov13 <AntonAleksandrov13@TeamCity>
2023-05-14 10:37:38 +0000

    TeamCity change in 'ITCourses / Devops101' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -63,7 +63,7 @@ object BuildDocker : BuildType({
         dockerCommand {
             name = "Push"
             commandType = push {
-                namesAndTags = "antonaleksandrovjetbrains/devops101app:test-2"
+                namesAndTags = "antonaleksandrovjetbrains/devops101app:latest"
             }
         }
     }
